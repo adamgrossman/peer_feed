14bd8d648e5f11f46ff7d60c45c7a0e3d145df48
Rudy Mutter <rudy@yeti.co>
2014-11-24 10:14:03 -0800

    Update utils.py

--- group_feed/utils.py
+++ group_feed/utils.py
@@ -1,5 +1,6 @@
 from rest_framework.authtoken.models import Token
 from group_feed.models import Member
 
+# Probably want to put this in a function and not have it run globally
 for member in Member.objects.all():
     Token.objects.get_or_create(user=member)
